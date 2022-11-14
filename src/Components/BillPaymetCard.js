4e2b56232b25595f4323c8135f6e935c8870d6ea
itachi1101 <aviral1101@gmail.com>
2022-11-14 21:39:00 +0530

    cal added

--- src/Components/BillPaymetCard.js
+++ src/Components/BillPaymetCard.js
@@ -1,22 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function BillPaymentCard(){
-    return(
+export default function BillPaymentCard() {
+    return (
         <View style={styles.container}>
-            <Text>
-                Bill Payment Card
+            <Text style={styles.heading}>
+                Pay Your Bill
             </Text>
+            <TouchableOpacity style={styles.payContainer}>
+                <Text style={styles.payText}>Pay Now</Text>
+            </TouchableOpacity>
         </View>
     )
 }
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#f58478', 
-        height:200,
-        borderRadius:20,
-        padding:20,
-        width:300,
-        marginLeft:30,
-        marginRight:30
+        backgroundColor: 'whitesmoke',
+        height: 200,
+        borderRadius: 20,
+        padding: 20,
+        width: 300,
+        marginLeft: 30,
+        marginRight: 30
+    },
+    heading:{
+        padding:10,
+        fontSize:25,
+        fontWeight:'bold',
+        letterSpacing:1.2,
+        marginBottom:30,
+        textAlign:"center"
+    },
+    payContainer:{
+        display:"flex",
+        justifyContent:"center",
+        padding:15,
+        backgroundColor:'coral',
+        borderRadius:15,
+    },
+    payText:{
+        fontSize:20,
+        color:"white",
+        textAlign:"center"
+
     }
 })
--- src/Screens/UserScreens/UserMainScreen.js
+++ src/Screens/UserScreens/UserMainScreen.js
@@ -1,31 +1,63 @@
 import { StatusBar } from "expo-status-bar";
-import { BackHandler, ScrollView, StyleSheet, Text, View } from "react-native";
+import { BackHandler, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native";
 import BillPaymentCard from "../../Components/BillPaymetCard";
+import CalendarComponent from "../../Components/CalanderComponent";
 import CustomStatusCard from "../../Components/CustomStatusCard";
 
 export default function UserMainScreen() {
     return (
         <View style={styles.container} >
-            <ScrollView horizontal={true} style={styles.scrollViewContainer}>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.scrollViewContainer}>
                 <CustomStatusCard />
                 <BillPaymentCard />
             </ScrollView>
+            <View style={styles.calendarContainer}>
+                <Text style={styles.calendarText}>
+                    Collection Record
+                </Text>
+                <CalendarComponent />
+            </View>
+            <TouchableOpacity style={styles.complainContainer}>
+                <Text style={styles.complainText}>
+                    Register Complaint
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1,
         backgroundColor: "#fff",
         paddingTop: StatusBar.currentHeight,
     },
     scrollViewContainer: {
-        width: "100%",
-        height: "80%",
         marginTop: 80,
+        flexGrow: 0,
+    },
+    calendarContainer:{
+        padding:20
+    }
+    ,calendarText:{
+        textAlign:"center",
+        padding:20,
+        fontSize:24,
+        fontWeight:'bold',
+        letterSpacing:1.2
+    }
+    ,
+    complainContainer: {
+        marginTop: 40,
+        backgroundColor: 'coral',
+        padding: 15,
+        marginLeft: 40,
+        marginRight: 40,
+        borderRadius: 18
+    },
+    complainText: {
+        textAlign: "center",
+        fontSize: 24,
+        color: "white",
+        fontWeight: 'bold'
     }
 })
